083501197baca0db901af6030754434886ee45ad
XiaoXin <jy0001087@gmail.com>
2022-09-19 17:55:45 +0800

    增加editText交互逻辑

--- app/src/main/java/com/rubbersheersock/amenity/ui/amenity/AmenityFragment.java
+++ app/src/main/java/com/rubbersheersock/amenity/ui/amenity/AmenityFragment.java
@@ -1,19 +1,20 @@
 package com.rubbersheersock.amenity.ui.amenity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.elvishew.xlog.XLog;
 import com.rubbersheersock.amenity.R;
-import com.rubbersheersock.amenity.databinding.FragmentAmenityBinding;
-import com.rubbersheersock.amenity.databinding.FragmentAmenityBinding;
 
 public class AmenityFragment extends Fragment {
 
@@ -28,12 +29,37 @@ public class AmenityFragment extends Fragment {
         TextView textView = rootView.findViewById(R.id.text_dashboard);
         amenityViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         //TODO:增加“发送”按钮提交功能
-        //TODO:增加输入框激活后自动清除内容功能
 
         Button sendButton = rootView.findViewById(R.id.morpheme_button);
 
 
+
         return rootView;
+    }
+
+    public void init(View rootView){
+        EditText editText = rootView.findViewById(R.id.morpheme_textInput);
+        //增加edittext清空事件
+        editText.setOnFocusChangeListener(
+                new View.OnFocusChangeListener(){
+                    @Override
+                    public void onFocusChange(View v, boolean hasFocus) {
+                        if(hasFocus == true){
+                            editText.setText("");
+                        }
+                    }
+                }
+        );
+        //增加提交按钮交互
+        editText.setOnEditorActionListener(
+                new TextView.OnEditorActionListener(){
+                    @Override
+                    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                        XLog.tag("MORPHEME--").i(event);
+                        return false;
+                    }
+                }
+        );
     }
 
     @Override
